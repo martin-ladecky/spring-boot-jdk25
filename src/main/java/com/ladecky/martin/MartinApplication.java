a385adbbcfed743c607e18afe2b0be6c1dfb1366
Ladecký Martin <martin.ladecky@airbank.cz>
2026-03-14 14:54:03 +0100

    Spring boot jdk 25 init project

---
+++ src/main/java/com/ladecky/martin/MartinApplication.java
@@ -0,0 +1,13 @@
+package com.ladecky.martin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ladecky/martin/MartinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladecky.martin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
